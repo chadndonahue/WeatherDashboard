8bb1e5ef3b330c09f0a410e159a9f84004d7295a
Chad <chadndonahue@gmail.com>
2022-01-12 20:42:02 -0500

    currently displaying name, wind, humidity, and temp on the page

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,5 +1,6 @@
 var submitButton = document.querySelector('#submitButton')
 var inputBox = document.querySelector('#cityName')
+
 var apiKey = "7fdbb9417197c505a863349af8396eb8"
 
 // Only need one call for uv index and 5 day report and need to pass long and lat 
@@ -12,8 +13,21 @@ function citySearch(e) {
     })
     .then((data)=>{
         console.log(data)
+        var name = document.querySelector('.name')
+        var wind = document.querySelector('.wind')
+        var humidity = document.querySelector('.humidity')
+        var temp = document.querySelector('.temp')
+        var uv = document.querySelector('.uv')
+
+        name.innerHTML = data.name
+        wind.innerHTML = data.wind.speed
+        humidity.innerHTML = data.main.humidity
+        temp.innerHTML = data.main.temp
         // fiveDay(data.coord.lat, data.coord.lon)
         // all the dom stuff for current conditions here
+    }) 
+    .catch((err)=>{
+        console.log(err)
     })
 } 
 
